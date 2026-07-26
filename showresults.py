99417ccdb5bf3d186e49b4c3d42eaede74a3b1e1
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-07-26 14:44:13 +1200

    Reinstate median line

--- showresults.py
+++ showresults.py
@@ -15,9 +15,9 @@ peak = np.empty(posterior_sample.shape[0])
 for i in range(posterior_sample.shape[0]):
     C, A, T, phi, mu, beta, L = posterior_sample[i, :]
     top = np.exp(C + A*np.sin(2.0*np.pi*t/T + phi))
-    mean = top/(1.0 + np.exp(-mu))
+    middle = top/(1.0 + np.exp(-mu))
     if i < 100:
-        plt.plot(t,  top, color="g", alpha=0.1)
-        plt.plot(t, mean, color="r", alpha=0.1)
+        plt.plot(t,    top, color="g", alpha=0.1)
+        plt.plot(t, middle, color="r", alpha=0.1)
 plt.show()
 
