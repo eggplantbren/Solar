14ed5e1b85d1a380d77d7014d300f82112e4f807
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-07-31 11:52:10 +1200

    Save simplified data for assignment

--- solar.R
+++ solar.R
@@ -5,5 +5,7 @@ t = t - min(t) + 1
 y = data$energy_kwh
 plot(t, y, type="l", ylim=c(0, 1.05*max(y)))
 
-write.table(data.frame(t=t, y=y), "data.txt", col.names=FALSE,
+df = data.frame(t=t, y=y)
+write.table(df, "data.txt", col.names=FALSE,
             row.names=FALSE)
+write.csv(df, "solar_simplified.csv", row.names=FALSE)
